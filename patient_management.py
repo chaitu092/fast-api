4460c79d665dc8500eadc0ff71bba3bf80c72568
chaitu092 <chaitu55088@gmail.com>
2025-05-17 22:19:18 +0200

    endpoint to retrive details of specific patient by id

--- patient_management.py
+++ patient_management.py
@@ -23,5 +23,24 @@ def patients_details():
 
 @app.get("/patient_view")
 def patient_view():
+    """
+    This endpoint retrieves the details of all patients.
+    :return: A dictionary containing a list of all patients.
+    """
     patient_data = load_patient_data()
     return {"patients": patient_data}
+
+
+@app.get("/patient/{patient_id}")
+def patient_view_by_id(patient_id: str):
+    """
+    Path parameters are dynamic segment values in the URL that can be used to retrieve specific resources.
+
+    This endpoint retrieves the details of a specific patient by their ID.
+    :param patient_id: The ID of the patient to retrieve.
+    :return: A dictionary containing the patient's details or an error message if not found.
+    """
+    patient_data = load_patient_data()
+    if patient_id in patient_data:
+        return patient_data[patient_id]
+    return {"Error Message": "Patient not found"}
