81640999bf7828aef99bf9f49a8ec694960e10a6
chaitu092 <chaitu55088@gmail.com>
2025-05-17 22:27:57 +0200

    added path functionality to add more details like description, example

--- patient_management.py
+++ patient_management.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -25,20 +25,24 @@ def patients_details():
 def patient_view():
     """
     This endpoint retrieves the details of all patients.
-    :return: A dictionary containing a list of all patients.
+    return: A dictionary containing a list of all patients.
     """
     patient_data = load_patient_data()
     return {"patients": patient_data}
 
 
 @app.get("/patient/{patient_id}")
-def patient_view_by_id(patient_id: str):
+def patient_view_by_id(
+    patient_id: str = Path(
+        ..., description="ID of the patient in the DB", example="P001"
+    )
+):
     """
     Path parameters are dynamic segment values in the URL that can be used to retrieve specific resources.
 
     This endpoint retrieves the details of a specific patient by their ID.
-    :param patient_id: The ID of the patient to retrieve.
-    :return: A dictionary containing the patient's details or an error message if not found.
+    param patient_id: The ID of the patient to retrieve.
+    return: A dictionary containing the patient's details or an error message if not found.
     """
     patient_data = load_patient_data()
     if patient_id in patient_data:
